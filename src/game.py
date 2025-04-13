c583ff1f8d62a9ef3b2e7be8ced93a0f28b58e7a
induarimilli <iarimill@andrew.cmu.edu>
2025-04-13 12:20:14 -0400

    game.py change

--- src/game.py
+++ src/game.py
@@ -8,9 +8,9 @@ class Player:
         self.r = radius
 
     def move(self, direction):
-        if direction == 'left' and self.x - self.r > 0:
+        if direction == 'left' and self.x - self.r > 100:
             self.x -= 5
-        elif direction == 'right' and self.x + self.r < 400:
+        elif direction == 'right' and self.x + self.r < 300:
             self.x += 5
 
     def draw(self):
@@ -23,24 +23,23 @@ class Coin:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.w = 20
-        self.h = 10
+        self.size = 10
 
     def move(self):
         self.y += 5
 
     def draw(self):
-        drawRect(self.x, self.y, self.w, self.h, fill='gold')
+        drawCircle(self.x, self.y, self.size, fill='gold')
 
     def getBounds(self):
-        return (self.x, self.y, self.x + self.w, self.y + self.h)
+        return (self.x - self.size, self.y - self.size, self.x + self.size, self.y + self.size)
 
 class Hole:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.w = 40
-        self.h = 20
+        self.w = 60
+        self.h = 30
 
     def move(self):
         self.y += 5
@@ -63,7 +62,8 @@ class Game:
         self.score = 0
         self.player = Player(200, 350, 15)
         self.coins = []
-        self.holes = []
+        self.hole = None  # Only one hole at a time
+        self.coinCooldown = 0
 
     def start(self):
         self.reset()
@@ -76,29 +76,47 @@ class Game:
     def update(self):
         if not self.started or self.over or self.paused:
             return
+        # Generate coins in vertical columns on the road
+        if self.coinCooldown <= 0:
+            x = random.randint(100, 300)  
+            for i in range(5):
+                self.coins.append(Coin(x, -i * 25))
+            self.coinCooldown = 40
+        else:
+            self.coinCooldown -= 1
 
-        # Spawn coins and holes
-        if random.random() < 0.03:
-            self.coins.append(Coin(random.randint(50, 350), 0))
-        if random.random() < 0.05:
-            self.holes.append(Hole(random.randint(50, 350), 0))
+        if self.hole is None and random.random() < 0.03:
+            self.hole = Hole(random.randint(120, 240), 0)
 
-        # Move items
         for coin in self.coins:
             coin.move()
-        for hole in self.holes:
-            hole.move()
+        if self.hole:
+            self.hole.move()
 
-        # Collision detection
         playerBounds = self.player.getBounds()
-        self.coins[:] = [c for c in self.coins if not self.checkCollision(playerBounds, c.getBounds()) or self.incrementScore(c)]
-        for hole in self.holes:
-            if self.checkCollision(playerBounds, hole.getBounds()):
-                self.over = True
 
-        # Remove off-screen
-        self.coins[:] = [c for c in self.coins if c.y < 400]
-        self.holes[:] = [h for h in self.holes if h.y < 400]
+        # Check is player got coins
+        updatedCoins = []
+        for coin in self.coins:
+            if self.checkCollision(playerBounds, coin.getBounds()):
+                self.incrementScore(coin)  
+            else:
+                updatedCoins.append(coin) 
+
+        # Remove coins that have gone off the screen 
+        validCoins = []
+        for coin in updatedCoins:
+            if coin.y < 400: # 400 is screen width 
+                validCoins.append(coin)  
+        #Adds coin to validCoins list
+        self.coins[:] = validCoins  
+
+        # Sprite falls in hole
+        if self.hole:
+            if self.checkCollision(playerBounds, self.hole.getBounds()):
+                self.over = True
+            elif self.hole.y > 400:
+                self.hole = None
 
     def incrementScore(self, coin):
         self.score += 1
@@ -113,22 +131,29 @@ class Game:
         bx1, by1, bx2, by2 = b
         return not (ax2 < bx1 or ax1 > bx2 or ay2 < by1 or ay1 > by2)
 
+    def drawRoadBackground(self):
+        drawRect(0, 0, 400, 400, fill='darkGreen')
+        # Used ChatGPT to generate road background - after Hack112, I will do it myself
+        for y in range(0, 400, 20):
+            offset = 10 if (y // 20) % 2 == 0 else 0
+            for x in range(100 + offset, 300, 20):
+                drawRect(x, y, 20, 20, fill='sienna', border='black', borderWidth=1)
+
     def draw(self):
         if not self.started:
             drawLabel('Temple Run', 200, 150, size=40, bold=True)
             drawRect(150, 200, 100, 40, fill='green')
             drawLabel('Start', 200, 220, size=20, fill='white')
-        elif self.paused:
-            drawLabel('Paused', 200, 200, size=20, fill='orange', bold=True)
         elif self.over:
             drawLabel(f'Game Over! Score: {self.score}', 200, 200, size=30, fill='red', bold=True)
         else:
+            self.drawRoadBackground()
             self.player.draw()
             for coin in self.coins:
                 coin.draw()
-            for hole in self.holes:
-                hole.draw()
-            drawLabel(f'Score: {self.score}', 200, 20, size=18)
+            if self.hole:
+                self.hole.draw()
+            drawLabel(f'Score: {self.score}', 200, 20, size=18, bold=True, fill="white")
 
 def onAppStart(app):
     app.game = Game(app)
